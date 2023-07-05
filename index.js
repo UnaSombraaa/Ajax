be6b5152dd3cf92742c9f16f0c58793b3cc6279b
danteuri <127241057+UnaSombraaa@users.noreply.github.com>
2023-07-05 14:37:25 -0300

    Ajax

--- index.js
+++ index.js
@@ -1,21 +1,78 @@
 
 function solicitudAJAX(params) {
-    var url = "https://api.thecatapi.com/v1/images/search?breed_ids=%7Bbreed.id%7D";
+    var url = "https://api.thecatapi.com/";
     var objXMLHttpRequest = new XMLHttpRequest();
     objXMLHttpRequest.onreadystatechange = function () {
-      if (objXMLHttpRequest.readyState === 4) {
-        if (objXMLHttpRequest.status === 200) {
-            console.log(objXMLHttpRequest.responseText);
-          let json = JSON.parse(objXMLHttpRequest.responseText);
-        
+      
+    new Vue({ 
+        el: '#app',
+        vuetify: new Vuetify(),
+        data: {
+            country_flag_url:"",
+          images: [],
+          breeds:[],
+          selected_breed: {},
+          current_image: {}
+        },
+        created(){
+            this.getBreeds();
+        } ,
+        watch: {
+           
+            selected_breed: function()
+            {
+                console.log(this.selected_breed)
 
-          console.dir(JSON);
-        } else {
-          alert("Error Code: " + objXMLHttpRequest.status);
-          alert("Error Message: " + objXMLHttpRequest.statusText);
+                let country_code = this.selected_breed.country_code.toLowerCase();
+
+                this.country_flag_url = `https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.1/flags/1x1/${country_code}.svg`;
+                this.getImages();
+            }
+        },
+        methods:{
+
+            async getBreeds()
+            {
+                try{
+                    axios.defaults.headers.common['x-api-key'] = "DEMO-API-KEY" 
+                    
+                    let response = await axios.get('https://api.thecatapi.com/v1/breeds/' ) 
+                    this.breeds = response.data;
+                    console.log("-- ("+this.breeds.length)
+                    
+                    this.selected_breed = this.breeds[10]
+                }catch(err){
+                    console.log(err)
+                }
+            },
+            async getImages()
+            {
+                try{
+                    
+                    let query_params = {
+                        breed_ids: this.selected_breed.id,
+                        limit:8
+                    }
+
+                    let response = await axios.get('https://api.thecatapi.com/v1/images/search', { params: query_params} ) 
+                    
+                    this.pagination_count = response.headers['pagination-count'];
+                    this.images = response.data;
+                    this.current_image = this.images[0];
+
+                    
+
+                    console.log("-- ("+this.images.length +") Images from TheCatAPI.com");
+                    console.log( this.pagination_count ,'images available for this query.');
+                    
+                }catch(err){
+                    console.log(err)
+                }
+            }
+            
         }
-      }
-    };
-    objXMLHttpRequest.open("GET", url + ".breeds");
-    objXMLHttpRequest.send();
+    }
+    
+  
+    )}
   }
